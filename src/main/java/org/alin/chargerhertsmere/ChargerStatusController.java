92c2a4916fb060e90744a55f2c9086ebe5e7497b
aneagu <alin.neagu@ardonaghspecialty.com>
2024-09-15 17:23:51 +0100

    call and status

--- src/main/java/org/alin/chargerhertsmere/ChargerStatusController.java
+++ src/main/java/org/alin/chargerhertsmere/ChargerStatusController.java
@@ -15,14 +15,20 @@ public class ChargerStatusController {
 
   private final ChargerStatusService chargerStatusService;
 
-  @PutMapping(path = "/api/call")
-  public void handleCall() {
+  @PutMapping(path = "/api/call", produces = MediaType.APPLICATION_JSON_VALUE)
+  public SwiftObject handleCall() {
     boolean status = chargerStatusService.isMakeCall();
     chargerStatusService.setMakeCall(!status);
+    return SwiftObject.builder().phone(!status).build();
+  }
+
+  @GetMapping(path = "/api/call", produces = MediaType.APPLICATION_JSON_VALUE)
+  public SwiftObject getObject() {
+    return SwiftObject.builder().phone(chargerStatusService.isMakeCall()).build();
   }
 
   @PostMapping(value = "/sms", produces = MediaType.APPLICATION_XML_VALUE)
-  public MessagingResponse smsWebhook(@RequestBody String body) {
+  public MessagingResponse smsWebhook() {
     boolean phone = chargerStatusService.changeStatus();
     return new MessagingResponse.Builder().message(new Message.Builder().body(new Body.Builder(String.format("Phone is now %s.", phone ? "on": "off")).build()).build()).build();
   }
--- src/main/java/org/alin/chargerhertsmere/SwiftObject.java
+++ src/main/java/org/alin/chargerhertsmere/SwiftObject.java
@@ -11,6 +11,5 @@ import lombok.NoArgsConstructor;
 @Builder
 public class SwiftObject {
 
-    private boolean email;
     private boolean phone;
 }
