b8f76268f707fe21646639d11504dc241e357b21
Alin Neagu <neagu_ionutalin@icloud.com>
2024-10-03 18:17:37 +0100

    history with any number of chargers

--- src/main/java/org/alin/chargerhertsmere/HistoryService.java
+++ src/main/java/org/alin/chargerhertsmere/HistoryService.java
@@ -3,31 +3,34 @@ package org.alin.chargerhertsmere;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HistoryService {
 
-  private static final Integer QUEUE_SIZE = 10;
-  @Getter private final List<String> queue = new ArrayList<>();
+    private static final Integer QUEUE_SIZE = 30;
+    @Getter
+    private final List<String> queue = new ArrayList<>();
 
-  public void addEntry(ResponseObject responseObject) {
-    if (queue.size() == QUEUE_SIZE) shiftQueueElements();
-    queue.add(
-        String.format(
-            "Time: %s. Charger %s is %s. Charger %s is %s.",
-            Instant.now(),
-            responseObject.getPods().get(0).getStatuses().get(0).getDoorId(),
-            responseObject.getPods().get(0).getStatuses().get(0).getLabel(),
-            responseObject.getPods().get(0).getStatuses().get(1).getDoorId(),
-            responseObject.getPods().get(0).getStatuses().get(1).getLabel()));
-  }
-
-  private void shiftQueueElements() {
-    for (int i = 1; i < QUEUE_SIZE; i++) {
-      queue.set(i - 1, queue.get(i));
+    public void addEntry(ResponseObject responseObject) {
+        if (queue.size() == QUEUE_SIZE) shiftQueueElements();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("Time: %s. ", Instant.now()));
+        for (int i = 0; i < responseObject.getPods().size(); i++) {
+            stringBuilder.append(String.format("C %s - %s. C %s - %s. ", responseObject.getPods().get(i).getStatuses().get(0).getDoorId(),
+                    responseObject.getPods().get(i).getStatuses().get(0).getLabel().charAt(0),
+                    responseObject.getPods().get(i).getStatuses().get(1).getDoorId(),
+                    responseObject.getPods().get(i).getStatuses().get(1).getLabel().charAt(0)));
+        }
+        queue.add(stringBuilder.toString());
     }
-    queue.remove(queue.get(QUEUE_SIZE-1));
-  }
+
+    private void shiftQueueElements() {
+        for (int i = 1; i < QUEUE_SIZE; i++) {
+            queue.set(i - 1, queue.get(i));
+        }
+        queue.remove(queue.get(QUEUE_SIZE - 1));
+    }
 }
