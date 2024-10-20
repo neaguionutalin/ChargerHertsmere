fb9929f5ad46cbf676a3a0a2114ecc7a77ade8ff
Alin Neagu <neagu_ionutalin@icloud.com>
2024-10-20 16:12:53 +0100

    history with any number of chargers

--- src/main/java/org/alin/chargerhertsmere/ChargerStatusService.java
+++ src/main/java/org/alin/chargerhertsmere/ChargerStatusService.java
@@ -8,10 +8,12 @@ import com.twilio.type.PhoneNumber;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.Properties;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -28,89 +30,75 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ChargerStatusService {
 
-  private static final String ACCOUNT_SID = "ACd9da66e6d9a951a280ee9bd384595fff";
+    private static final String ACCOUNT_SID = "ACd9da66e6d9a951a280ee9bd384595fff";
 
-  @Value("${app.auth_token}")
-  private String AUTH_TOKEN;
-  @Value("${app.email_password}")
-  private String EMAIL_TOKEN;
+    @Value("${app.auth_token}")
+    private String AUTH_TOKEN;
+    @Value("${app.email_password}")
+    private String EMAIL_TOKEN;
 
-  private final RestTemplate restTemplate;
-  private final HistoryService historyService;
-  private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-  @Getter private ResponseObject activeResponseObject = null;
-  @Getter @Setter private boolean makeCall = false;
+    private final RestTemplate restTemplate;
+    private final HistoryService historyService;
+    private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    @Getter
+    private ResponseObject activeResponseObject = null;
+    @Getter
+    @Setter
+    private boolean makeCall = false;
 
-  public boolean changeStatus() {
-    this.makeCall = !this.makeCall;
-    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-    com.twilio.rest.api.v2010.account.Message message =
-        com.twilio.rest.api.v2010.account.Message.creator(
-                new com.twilio.type.PhoneNumber("+447490927845"),
-                new com.twilio.type.PhoneNumber("+447588667442"),
-                String.format("Phone is now %s.", this.makeCall? "on": "off"))
-            .create();
-    log.info("Changed Status: {}", message.getBody());
-    return this.makeCall;
-  }
-
-  @Scheduled(cron = "0 0/1 * * * ?")
-  @EventListener(ApplicationStartedEvent.class)
-  public void checkStatus() throws JsonProcessingException, URISyntaxException {
-    ResponseObject response =
-        restTemplate.getForObject(
-            "https://charge.pod-point.com/ajax/pods/1545", ResponseObject.class);
-    if (!Objects.equals(response, activeResponseObject)) historyService.addEntry(response);
-    if (!Objects.equals(response, activeResponseObject) && this.makeCall) {
-      String to = "ineagu01@mail.bbk.ac.uk";
-      String from = "neagu_ionutalin@icloud.com";
-      String host = "smtp.mail.me.com";
-      Properties properties = System.getProperties();
-
-      properties.put("mail.smtp.host", host);
-      properties.put("mail.smtp.port", "587");
-      properties.put("mail.smtp.starttls.required", "true");
-      properties.put("mail.smtp.auth", "true");
-
-      Session session =
-          Session.getInstance(
-              properties,
-              new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                  return new PasswordAuthentication(from, EMAIL_TOKEN);
-                }
-              });
-      try {
-        // Create a default MimeMessage object.
-        MimeMessage message = new MimeMessage(session);
-        // Set From: header field of the header.
-        message.setFrom(new InternetAddress(from));
-        // Set To: header field of the header.
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-        // Set Subject: header field
-        message.setSubject(
-            String.format(
-                "Charge A %s, Charger B %s",
-                response.getPods().get(0).getStatuses().get(0).getLabel(),
-                response.getPods().get(0).getStatuses().get(1).getLabel()));
-        // Now set the actual message
-        message.setText(OBJECT_MAPPER.writeValueAsString(response));
-        log.info("sending...");
-        // Send message
-        Transport.send(message);
-        log.info("Sent message successfully....");
-      } catch (MessagingException mex) {
-        mex.printStackTrace();
-      }
-      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-      Call call =
-          Call.creator(
-                  new PhoneNumber("+447490927845"),
-                  new PhoneNumber("+447588667442"),
-                  new URI("http://demo.twilio.com/docs/voice.xml"))
-              .create();
-      log.info("Call made: {}", call.getStatus());
+    public boolean changeStatus() {
+        this.makeCall = !this.makeCall;
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        com.twilio.rest.api.v2010.account.Message message = com.twilio.rest.api.v2010.account.Message.creator(new com.twilio.type.PhoneNumber("+447490927845"), new com.twilio.type.PhoneNumber("+447588667442"), String.format("Phone is now %s.", this.makeCall ? "on" : "off")).create();
+        log.info("Changed Status: {}", message.getBody());
+        return this.makeCall;
     }
-    activeResponseObject = response;
-  }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    @EventListener(ApplicationStartedEvent.class)
+    public void checkStatus() throws JsonProcessingException, URISyntaxException {
+        ResponseObject response = restTemplate.getForObject("https://charge.pod-point.com/ajax/pods/1545", ResponseObject.class);
+        if (response == null) return;
+        response.setPods(response.getPods().stream().sorted().peek(pod -> pod.setStatuses(pod.getStatuses().stream().sorted().toList())).toList());
+        if (!Objects.equals(response, activeResponseObject)) historyService.addEntry(response);
+        if (!Objects.equals(response, activeResponseObject) && this.makeCall) {
+            String to = "ineagu01@mail.bbk.ac.uk";
+            String from = "neagu_ionutalin@icloud.com";
+            String host = "smtp.mail.me.com";
+            Properties properties = System.getProperties();
+
+            properties.put("mail.smtp.host", host);
+            properties.put("mail.smtp.port", "587");
+            properties.put("mail.smtp.starttls.required", "true");
+            properties.put("mail.smtp.auth", "true");
+
+            Session session = Session.getInstance(properties, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(from, EMAIL_TOKEN);
+                }
+            });
+            try {
+                // Create a default MimeMessage object.
+                MimeMessage message = new MimeMessage(session);
+                // Set From: header field of the header.
+                message.setFrom(new InternetAddress(from));
+                // Set To: header field of the header.
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                // Set Subject: header field
+                message.setSubject(String.format("Charge A %s, Charger B %s", response.getPods().get(0).getStatuses().get(0).getLabel(), response.getPods().get(0).getStatuses().get(1).getLabel()));
+                // Now set the actual message
+                message.setText(OBJECT_MAPPER.writeValueAsString(response));
+                log.info("sending...");
+                // Send message
+                Transport.send(message);
+                log.info("Sent message successfully....");
+            } catch (MessagingException mex) {
+                mex.printStackTrace();
+            }
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Call call = Call.creator(new PhoneNumber("+447490927845"), new PhoneNumber("+447588667442"), new URI("http://demo.twilio.com/docs/voice.xml")).create();
+            log.info("Call made: {}", call.getStatus());
+        }
+        activeResponseObject = response;
+    }
 }
--- src/main/java/org/alin/chargerhertsmere/ResponseObject.java
+++ src/main/java/org/alin/chargerhertsmere/ResponseObject.java
@@ -1,6 +1,5 @@
 package org.alin.chargerhertsmere;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -8,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -18,26 +18,37 @@ import java.util.List;
 public class ResponseObject {
 
     private List<Pod> pods;
+
     @Data
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Pod {
+    public static class Pod implements Comparator<Pod> {
 
         private int id;
         private List<Status> statuses;
+
+        @Override
+        public int compare(Pod o1, Pod o2) {
+            return o1 != null && o2 != null ? o1.id < o2.id ? 1 : -1 : 0;
+        }
 
         @Data
         @Builder
         @AllArgsConstructor
         @NoArgsConstructor
         @JsonIgnoreProperties(ignoreUnknown = true)
-        public static class Status {
+        public static class Status implements Comparator<Status> {
             @JsonProperty("door_id")
             private Integer doorId;
             private String label;
             private ActiveCharge activeCharge;
+
+            @Override
+            public int compare(Status o1, Status o2) {
+                return o1 != null && o2 != null ? o1.doorId < o2.doorId ? 1 : -1 : 0;
+            }
 
             @Data
             @Builder
